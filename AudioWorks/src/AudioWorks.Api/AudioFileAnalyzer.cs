521265c99f05505acd605db893fd04aa7a03bebb
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-02-11 15:37:31 -0500

    Removed unused field.

--- AudioWorks/src/AudioWorks.Api/AudioFileAnalyzer.cs
+++ AudioWorks/src/AudioWorks.Api/AudioFileAnalyzer.cs
@@ -34,7 +34,6 @@ namespace AudioWorks.Api
     public sealed class AudioFileAnalyzer
     {
         [NotNull] readonly ExportFactory<IAudioAnalyzer> _analyzerFactory;
-        [NotNull] readonly string _progressDescription;
         int _maxDegreeOfParallelism = Environment.ProcessorCount;
 
         /// <summary>
@@ -80,8 +79,6 @@ namespace AudioWorks.Api
 
             using (var export = _analyzerFactory.CreateExport())
                 Settings = new ValidatingSettingDictionary(export.Value.SettingInfo, settings);
-
-            _progressDescription = $"Performing {name} analysis";
         }
 
         /// <summary>
