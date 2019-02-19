2fc86e04865a5d2c7d70bab1319811cab4d126d2
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-02-18 20:53:27 -0500

    Added WPF- and Prism-based "UI" project scaffolding (Windows-only)

---
+++ AudioWorks/src/AudioWorks.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿/* Copyright © 2019 Jeremy Herbison
+
+This file is part of AudioWorks.
+
+AudioWorks is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+AudioWorks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
+<https://www.gnu.org/licenses/>. */
+
+using System.Windows;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace AudioWorks.UI.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title { get; } = "AudioWorks";
+
+        public DelegateCommand ExitCommand { get; } = new DelegateCommand(() => Application.Current.Shutdown());
+    }
+}
