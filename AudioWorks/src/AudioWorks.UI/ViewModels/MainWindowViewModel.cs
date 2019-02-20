87c93dd42ee763dc6187d335c4e67a12b752c77e
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-02-20 11:15:08 -0500

    Just bind to an ObservableCollection for now (simplifies things).

--- AudioWorks/src/AudioWorks.UI/ViewModels/MainWindowViewModel.cs
+++ AudioWorks/src/AudioWorks.UI/ViewModels/MainWindowViewModel.cs
@@ -14,9 +14,8 @@ You should have received a copy of the GNU Affero General Public License along w
 <https://www.gnu.org/licenses/>. */
 
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Windows.Data;
 using AudioWorks.Api;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -26,7 +25,7 @@ namespace AudioWorks.UI.ViewModels
     // ReSharper disable once UnusedMember.Global
     public class MainWindowViewModel : BindableBase
     {
-        public ListCollectionView AudioFiles { get; } = new ListCollectionView(new List<TaggedAudioFile>());
+        public ObservableCollection<TaggedAudioFile> AudioFiles { get; } = new ObservableCollection<TaggedAudioFile>();
 
         public DelegateCommand SelectFilesCommand { get; }
 
@@ -39,14 +38,11 @@ namespace AudioWorks.UI.ViewModels
                 var newFiles = fileSelectionService.SelectFiles().ToList();
 
                 // Skip files that were added previously
-                foreach (var existingFile in ((List<TaggedAudioFile>) AudioFiles.SourceCollection)
-                    .Select(audioFile => audioFile.Path))
+                foreach (var existingFile in AudioFiles.Select(audioFile => audioFile.Path))
                     if (newFiles.Contains(existingFile, StringComparer.OrdinalIgnoreCase))
                         newFiles.Remove(existingFile);
 
-                // Parse the audio files asynchronously
-                foreach (var newAudioFile in newFiles.Select(file => new TaggedAudioFile(file)))
-                    AudioFiles.AddNewItem(newAudioFile);
+                AudioFiles.AddRange(newFiles.Select(file => new TaggedAudioFile(file)));
             });
 
             ExitCommand = new DelegateCommand(appShutdownService.Shutdown);
