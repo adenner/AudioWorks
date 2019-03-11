ca82cd3e2a99b44d95813322b5ff2fc2601b1d2c
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-03-11 10:25:13 -0400

    Remove unneeded using statement.

--- AudioWorks/src/AudioWorks.UI/Views/MainWindow.xaml.cs
+++ AudioWorks/src/AudioWorks.UI/Views/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ details.
 You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
 <https://www.gnu.org/licenses/>. */
 
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
