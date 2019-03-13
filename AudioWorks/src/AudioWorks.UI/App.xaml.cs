f3b8160174af6ef3432c4db2c2e56ce298d0c19d
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-03-12 23:27:18 -0400

    Save and Load metadata settings to/from disk at launch and exit.

--- AudioWorks/src/AudioWorks.UI/App.xaml.cs
+++ AudioWorks/src/AudioWorks.UI/App.xaml.cs
@@ -48,7 +48,16 @@ namespace AudioWorks.UI
             Fluent.ThemeManager.IsAutomaticWindowsAppModeSettingSyncEnabled = true;
             Fluent.ThemeManager.SyncThemeWithWindowsAppModeSetting();
 
+            SettingManager.LoadFromDisk();
+
             base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            SettingManager.SaveToDisk();
+
+            base.OnExit(e);
         }
     }
 }
--- AudioWorks/src/AudioWorks.UI/SettingManager.cs
+++ AudioWorks/src/AudioWorks.UI/SettingManager.cs
@@ -13,15 +13,43 @@ details.
 You should have received a copy of the GNU Affero General Public License along with AudioWorks. If not, see
 <https://www.gnu.org/licenses/>. */
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using AudioWorks.Common;
+using Newtonsoft.Json;
 
 namespace AudioWorks.UI
 {
     static class SettingManager
     {
+        static readonly string _settingsPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "AudioWorks",
+            "UI",
+            "Settings");
+
         //TODO initialize this properly
         internal static IDictionary<string, SettingDictionary> MetadataEncoderSettings { get; } =
             new Dictionary<string, SettingDictionary> { ["FLAC"] = new SettingDictionary() };
+
+        internal static void SaveToDisk()
+        {
+            Directory.CreateDirectory(_settingsPath);
+
+            foreach (var (format, settings) in MetadataEncoderSettings)
+                using (var writer = new StreamWriter(Path.Combine(_settingsPath, $"{format}.json")))
+                    writer.Write(JsonConvert.SerializeObject(settings));
+        }
+
+        internal static void LoadFromDisk()
+        {
+            if (!Directory.Exists(_settingsPath)) return;
+
+            foreach (var file in Directory.EnumerateFiles(_settingsPath, "*.json"))
+                using (var reader = new StreamReader(file))
+                    MetadataEncoderSettings[Path.GetFileNameWithoutExtension(file)] =
+                        JsonConvert.DeserializeObject<SettingDictionary>(reader.ReadToEnd(), new SettingDictionaryConverter());
+        }
     }
 }
